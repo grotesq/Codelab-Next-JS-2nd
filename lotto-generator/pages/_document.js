10fd30805e4b0e17ca80752b6e7ec8da58a39c5c
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2019-09-08 11:46:44 +0900

    update lotto-generator

---
+++ lotto-generator/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link rel="stylesheet" href="/static/common.css"/>
+        </Head>
+        <body>
+          <div id="root">
+            <Main />
+            <NextScript />
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
--- lotto-generator/pages/index.js
+++ lotto-generator/pages/index.js
@@ -4,68 +4,54 @@ import Head from 'next/head';
 import Nav from '../components/nav';
 import _ from 'underscore';
 
-let numbers = [];
-_.times( 45, n => numbers.push( n + 1 ) );
-numbers = _.shuffle( numbers );
-numbers.length = 6;
+function generate() {
+  let numbers = [];
+  _.times( 45, n => numbers.push( n + 1 ) );
+  numbers = _.shuffle( numbers );
+  numbers.length = 6;
+  return numbers;
+}
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-    </Head>
+const Home = () => {
+  const [ list, setList ] = React.useState( [] );
+  const regenerate = () => {
+    // state에 값 넣기
+    setList( generate() );
+  }
+  React.useEffect( () => {
+    // mount, update
+    regenerate();
+  }, [] ); // 주시할 대상
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
 
-    <div className='hero'>
-      { numbers.join( ',' ) }
+      <div className='container'>
+        <div>
+          { list.map( element => <span>{ element + ' ' }</span> ) }
+        </div>
+
+        <div>
+          <button onClick={ regenerate }>재생성</button>
+          {/* <button onClick={ () => setList( generate() ) }>재생성</button> */}
+        </div>
+      </div>
+
+      {/* scope */}
+      <style jsx>{`
+        .container {
+          width: 100vw;
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
     </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+  )
+}
 
 export default Home
